bf7d2b7ef33d99f63ba3c2e18c5bc63666f2da3b
Ross <jross0@jross0-003532.home>
2017-07-30 22:49:50 -0400

    Load connection information based on sails version

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ var root   = argv.path || process.cwd();
 var Schema = require('./lib/schema');
 var path   = require('path');
 var extend = require('extend');
+var file = parseInt(require('child_process').execSync('sails -v')) < 1 ? 'connections' : 'datastores';
 var config = extend(
-  require(path.join(root, 'config/connections.js')),
-  require(path.join(root, 'config/models.js')),
-  require(path.join(root, 'config/local.js'))
+    require(path.join(root, 'config/'+ file +'.js')),
+    require(path.join(root, 'config/models.js')),
+    require(path.join(root, 'config/local.js'))
 );
 argv.path  = root;
 var knex   = require('./lib/connection')(config, argv.connection);
 var schema = new Schema(knex, argv);
 
 schema
-  .getSchema(argv.table, argv.database || knex.client.connectionSettings.database)
-  .then(schemas => {
-    return schema.write(argv.table, schemas);
-  })
-  .then(() => {
-    knex.destroy(() => {
-      console.log('> All done! Have fun with your models. :)');
-    });
-  }).catch(console.error);
+    .getSchema(argv.table, argv.database || knex.client.connectionSettings.database)
+    .then(schemas => {
+        return schema.write(argv.table, schemas);
+    })
+    .then(() => {
+        knex.destroy(() => {
+            console.log('> All done! Have fun with your models. :)');
+        });
+    }).catch(console.error);
